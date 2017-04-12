cfbd444b0578001a4c132ccf357afa658fb0b744
Long Yun <longdeqidao@gmail.com>
2017-04-12 12:45:38 +0800

    [script]: adapt apm scripts to new easegateway client

--- src/inventory/script/init_apm_api.sh
+++ src/inventory/script/init_apm_api.sh
@@ -5,11 +5,11 @@ echo "SCRIPTFILE: ${SCRIPTFILE}"
 SCRIPTPATH="$(dirname "$SCRIPTFILE")"
 echo "SCRIPTPATH: ${SCRIPTPATH}"
 
-ADMIN=${SCRIPTPATH}/../../bin/easegateway_admin
+CLIENT=${SCRIPTPATH}/../../bin/easegateway-client
 
-HOST="$1"
-if [ -z "${HOST}" ]; then
-    KAFKA_BOOTSTRAP_SERVERS='127.0.0.1:9090'
+ADDRESS="$1"
+if [ -z "${ADDRESS}" ]; then
+    ADDRESS='127.0.0.1:9090'
 fi
 
 KAFKA_BOOTSTRAP_SERVERS="$2"
@@ -22,8 +22,8 @@ echo "Initial APM Plugins"
 rm -fr ${SCRIPTPATH}/apm_plugins
 cp -r ${SCRIPTPATH}/apm_plugins_template ${SCRIPTPATH}/apm_plugins
 sed -i "s#KAFKA_BOOTSTRAP_SERVERS#"${KAFKA_BOOTSTRAP_SERVERS}"#g" ${SCRIPTPATH}/apm_plugins/*.json
-${ADMIN} --host "$HOST" plugin add ${SCRIPTPATH}/apm_plugins/*.json
+${CLIENT} --address "${ADDRESS}" admin plugin add ${SCRIPTPATH}/apm_plugins/*.json
 
 echo ""
 echo "Initial APM Pipelines"
-${ADMIN} --host "$HOST" pipeline add ${SCRIPTPATH}/apm_pipelines_template/*.json
+${CLIENT} --address "${ADDRESS}" admin pipeline add ${SCRIPTPATH}/apm_pipelines_template/*.json
