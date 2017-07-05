335fc0597c4868a20bcd74c6389c1e725bb9bd2c
Yun Long <longdeqidao@gmail.com>
2017-07-05 21:16:03 +0800

    [cli]: implement cluster pipeline administration APIs

--- src/cli/cluster_admin.go
+++ src/cli/cluster_admin.go
@@ -1,12 +1,13 @@
 package cli
 
 import (
-	"common"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"strings"
 	"time"
+
+	"common"
 
 	"github.com/hexdecteam/easegateway-go-client/rest/1.0/cluster/admin/v1/pdu"
 	"github.com/urfave/cli"
@@ -147,8 +148,10 @@ func ClusterCreatePlugin(c *cli.Context) error {
 
 		setLocalOperationSequence(group, seq)
 
-		if timeout <= expiredTime && i < len(args)-1 {
-			errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+			}
 			break
 		}
 		timeout -= expiredTime
@@ -206,8 +209,10 @@ func ClusterDeletePlugin(c *cli.Context) error {
 		do(pluginName, seq)
 		setLocalOperationSequence(group, seq+1)
 		expiredTime = time.Now().Sub(startTime)
-		if timeout <= expiredTime && i < len(args)-1 {
-			errs.append(fmt.Errorf("timeout: no time to handle: %s", args[i+1:]))
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: no time to handle: %s", args[i+1:]))
+			}
 			break
 		}
 		timeout -= expiredTime
@@ -279,8 +284,10 @@ func ClusterRetrievePlugins(c *cli.Context) error {
 			startTime := time.Now()
 			do(pluginName)
 			expiredTime := time.Now().Sub(startTime)
-			if timeout <= expiredTime && i < len(args)-1 {
-				errs.append(fmt.Errorf("timeout: skip to handle: %s", args[i+1:]))
+			if timeout <= expiredTime {
+				if i < len(args)-1 {
+					errs.append(fmt.Errorf("timeout: skip to handle: %s", args[i+1:]))
+				}
 				break
 			}
 			timeout -= expiredTime
@@ -354,8 +361,10 @@ func ClusterUpdatePlugin(c *cli.Context) error {
 
 		setLocalOperationSequence(group, seq)
 
-		if timeout <= expiredTime && i < len(args)-1 {
-			errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+			}
 			break
 		}
 		timeout -= expiredTime
@@ -365,25 +374,340 @@ func ClusterUpdatePlugin(c *cli.Context) error {
 }
 
 func ClusterCreatePipeline(c *cli.Context) error {
-	return nil
+	args := c.Args()
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	timeout := time.Duration(timeoutSec) * time.Second
+	consistent := c.GlobalBool("consistent")
+
+	errs := &multipleErr{}
+
+	do := func(source string, seq uint64, data []byte) {
+		req := new(pdu.PipelineCreationClusterRequest)
+		err := json.Unmarshal(data, req)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", source, err))
+			return
+		}
+
+		req.TimeoutSec = uint16(timeout.Seconds())
+		req.Consistent = consistent
+		req.OperationSeq = seq
+
+		resp, err := clusterAdminApi().CreatePipeline(group, req)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", source, err))
+			return
+		} else if resp.Error != nil {
+			errs.append(fmt.Errorf("%s: %s", source, resp.Error.Error))
+			return
+		}
+	}
+
+	if len(args) == 0 {
+		args = append(args, "/dev/stdin")
+	}
+	for i, file := range args {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", file, err))
+			continue
+		}
+
+		startTime := time.Now()
+		seq, err := getOperationSequence(group, uint16(timeout.Seconds()))
+		expiredTime := time.Now().Sub(startTime)
+
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", file, err))
+			break
+		}
+
+		if timeout <= expiredTime {
+			errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i:], ", ")))
+			break
+		}
+		timeout -= expiredTime
+
+		seq++
+
+		startTime = time.Now()
+		do(file, seq, data)
+		expiredTime = time.Now().Sub(startTime)
+
+		setLocalOperationSequence(group, seq)
+
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+			}
+			break
+		}
+		timeout -= expiredTime
+	}
+
+	return errs.Return()
 }
 
 func ClusterDeletePipeline(c *cli.Context) error {
-	return nil
+	args := c.Args()
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	timeout := time.Duration(timeoutSec) * time.Second
+	consistent := c.GlobalBool("consistent")
+
+	errs := &multipleErr{}
+
+	do := func(pipelineName string, seq uint64) {
+		req := new(pdu.ClusterOperationRequest)
+		req.TimeoutSec = uint16(timeout.Seconds())
+		req.Consistent = consistent
+		req.OperationSeq = seq
+		resp, err := clusterAdminApi().DeletePipelineByName(group, pipelineName, req)
+
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", pipelineName, err))
+			return
+		} else if resp.Error != nil {
+			errs.append(fmt.Errorf("%s: %s", pipelineName, resp.Error.Error))
+			return
+		}
+	}
+
+	if len(args) == 0 {
+		errs.append(fmt.Errorf("pipeline name requied"))
+		return errs.Return()
+	}
+	for i, pipelineName := range args {
+		startTime := time.Now()
+		seq, err := getOperationSequence(group, uint16(timeout.Seconds()))
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", pipelineName, err))
+			break
+		}
+		expiredTime := time.Now().Sub(startTime)
+		if timeout <= expiredTime {
+			errs.append(fmt.Errorf("timeout: no time to handle", args[i:]))
+			break
+		}
+		timeout -= expiredTime
+
+		seq++
+
+		startTime = time.Now()
+		do(pipelineName, seq)
+		setLocalOperationSequence(group, seq+1)
+		expiredTime = time.Now().Sub(startTime)
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: no time to handle: %s", args[i+1:]))
+			}
+			break
+		}
+		timeout -= expiredTime
+	}
+
+	return errs.Return()
 }
 
 func ClusterRetrievePipelines(c *cli.Context) error {
-	return nil
+	args := c.Args()
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	timeout := time.Duration(timeoutSec) * time.Second
+	consistent := c.GlobalBool("consistent")
+
+	errs := &multipleErr{}
+
+	do := func(pipelineName string) {
+		req := new(pdu.ClusterRetrieveRequest)
+		req.TimeoutSec = uint16(timeout.Seconds())
+		req.Consistent = consistent
+		retrieveResp, apiResp, err := clusterAdminApi().GetPipelineByName(group, pipelineName, req)
+
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", pipelineName, err))
+			return
+		} else if apiResp.Error != nil {
+			errs.append(fmt.Errorf("%s: %s", pipelineName, apiResp.Error.Error))
+			return
+		}
+
+		data, err := json.Marshal(retrieveResp)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", pipelineName, err))
+			return
+		}
+
+		// TODO: make it pretty
+		fmt.Printf("%s\n", data)
+	}
+
+	doAll := func() {
+		req := new(pdu.PipelinesRetrieveClusterRequest)
+		req.TimeoutSec = uint16(timeout.Seconds())
+		req.Consistent = consistent
+		retrieveResp, apiResp, err := clusterAdminApi().GetPipelines(group, req)
+		if err != nil {
+			errs.append(fmt.Errorf("%v", err))
+			return
+		} else if apiResp.Error != nil {
+			errs.append(fmt.Errorf("%s", apiResp.Error.Error))
+			return
+		}
+
+		data, err := json.Marshal(retrieveResp)
+		if err != nil {
+			errs.append(fmt.Errorf("%v", err))
+			return
+		}
+
+		// TODO: make it pretty
+		fmt.Printf("%s\n", data)
+	}
+
+	if len(args) == 0 {
+		doAll()
+	} else {
+		for i, pipelineName := range args {
+			startTime := time.Now()
+			do(pipelineName)
+			expiredTime := time.Now().Sub(startTime)
+			if timeout <= expiredTime {
+				if i < len(args)-1 {
+					errs.append(fmt.Errorf("timeout: skip to handle: %s", args[i+1:]))
+				}
+				break
+			}
+			timeout -= expiredTime
+		}
+	}
+
+	return errs.Return()
 }
 
 func ClusterUpdatePipeline(c *cli.Context) error {
-	return nil
+	args := c.Args()
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	timeout := time.Duration(timeoutSec) * time.Second
+	consistent := c.GlobalBool("consistent")
+
+	errs := &multipleErr{}
+
+	do := func(source string, seq uint64, data []byte) {
+		req := new(pdu.PipelineUpdateClusterRequest)
+		err := json.Unmarshal(data, req)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", source, err))
+			return
+		}
+
+		req.TimeoutSec = uint16(timeout.Seconds())
+		req.Consistent = consistent
+		req.OperationSeq = seq
+
+		resp, err := clusterAdminApi().UpdatePipeline(group, req)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", source, err))
+			return
+		} else if resp.Error != nil {
+			errs.append(fmt.Errorf("%s: %s", source, resp.Error.Error))
+			return
+		}
+	}
+
+	if len(args) == 0 {
+		args = append(args, "/dev/stdin")
+	}
+	for i, file := range args {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", file, err))
+			continue
+		}
+
+		startTime := time.Now()
+		seq, err := getOperationSequence(group, uint16(timeout.Seconds()))
+		expiredTime := time.Now().Sub(startTime)
+
+		if err != nil {
+			errs.append(fmt.Errorf("%s: %v", file, err))
+			break
+		}
+
+		if timeout <= expiredTime {
+			errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i:], ", ")))
+			break
+		}
+		timeout -= expiredTime
+
+		seq++
+
+		startTime = time.Now()
+		do(file, seq, data)
+		expiredTime = time.Now().Sub(startTime)
+
+		setLocalOperationSequence(group, seq)
+
+		if timeout <= expiredTime {
+			if i < len(args)-1 {
+				errs.append(fmt.Errorf("timeout: skip to handle [%s]", strings.Join(args[i+1:], ", ")))
+			}
+			break
+		}
+		timeout -= expiredTime
+	}
+
+	return errs.Return()
 }
 
 func ClusterRetrievePluginTypes(c *cli.Context) error {
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	consistent := c.GlobalBool("consistent")
+
+	req := new(pdu.ClusterRetrieveRequest)
+	req.TimeoutSec = timeoutSec
+	req.Consistent = consistent
+	retrieveResp, apiResp, err := clusterAdminApi().GetPluginTypes(group, req)
+	if err != nil {
+		return err
+	} else if apiResp.Error != nil {
+		return fmt.Errorf("%s", apiResp.Error.Error)
+	}
+
+	data, err := json.Marshal(retrieveResp)
+	if err != nil {
+		return err
+	}
+
+	// TODO: make it pretty
+	fmt.Printf("%s\n", data)
 	return nil
 }
 
 func ClusterRetrievePipelineTypes(c *cli.Context) error {
+	group := c.GlobalString("group")
+	timeoutSec := uint16(*c.GlobalGeneric("timeout").(*common.Uint16Value))
+	consistent := c.GlobalBool("consistent")
+
+	req := new(pdu.ClusterRetrieveRequest)
+	req.TimeoutSec = timeoutSec
+	req.Consistent = consistent
+	retrieveResp, apiResp, err := clusterAdminApi().GetPipelineTypes(group, req)
+	if err != nil {
+		return err
+	} else if apiResp.Error != nil {
+		return fmt.Errorf("%s", apiResp.Error.Error)
+	}
+
+	data, err := json.Marshal(retrieveResp)
+	if err != nil {
+		return err
+	}
+
+	// TODO: make it pretty
+	fmt.Printf("%s\n", data)
 	return nil
 }
