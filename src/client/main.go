eb9a1cba21b1cf7f0b3ed5ef5bc77f2df7de3cbc
Yun Long <longdeqidao@gmail.com>
2017-07-06 11:16:11 +0800

    [cli]: reduce visual depth of commands - better readability

--- src/client/main.go
+++ src/client/main.go
@@ -60,359 +60,8 @@ func main() {
 	}
 
 	app.Commands = []urfavecli.Command{
-		{
-			Name:  "admin",
-			Usage: "Administration interface",
-			Subcommands: []urfavecli.Command{
-				{
-					Name:  "plugin",
-					Usage: "Plugin administration interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "add",
-							// TODO: add -f --force to overwrite existing plugins
-							Usage:  "Create one or more plugins",
-							Action: cli.CreatePlugin,
-						},
-						{
-							Name:   "rm",
-							Usage:  "Delete one or more plugins",
-							Action: cli.DeletePlugin,
-						},
-						{
-							Name: "ls",
-							// TODO: add --only-name -n
-							// TODO: add --format (json, xml...)
-							// TODO: add --type -t
-							Usage:  "Retrieve plugins",
-							Action: cli.RetrievePlugins,
-						},
-						{
-							Name: "update",
-							// TODO: add -f --force to add plugins which does not exist
-							Usage:  "Update one or more plugins",
-							Action: cli.UpdatePlugin,
-						},
-						{
-							Name:   "types",
-							Usage:  "Retrieve plugin types",
-							Action: cli.RetrievePluginTypes,
-						},
-					},
-				},
-				{
-					Name:  "pipeline",
-					Usage: "Pipeline administration interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "add",
-							// TODO: add -f --force to overwrite existing pipelines
-							Usage:  "Create one or more pipelines",
-							Action: cli.CreatePipeline,
-						},
-						{
-							Name:   "rm",
-							Usage:  "Delete one or more pipelines",
-							Action: cli.DeletePipeline,
-						},
-						{
-							Name: "ls",
-							// TODO: add -n --only-name
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipelines",
-							Action: cli.RetrievePipelines,
-						},
-						{
-							Name: "update",
-							// TODO: add -f --force to add pipeline which does not exist
-							Usage:  "Update one or more pipelines",
-							Action: cli.UpdatePipeline,
-						},
-						{
-							Name:   "types",
-							Usage:  "Retrieve pipeline types",
-							Action: cli.RetrievePipelineTypes,
-						},
-					},
-				},
-			},
-		},
-		{
-			Name:  "stat",
-			Usage: "Statistics interface",
-			Subcommands: []urfavecli.Command{
-				{
-					Name:  "plugin",
-					Usage: "Plugin statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve plugin indicator names",
-							Action: cli.RetrievePluginIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve plugin indicator value",
-							Action: cli.GetPluginIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve plugin indicator description",
-							Action: cli.GetPluginIndicatorDesc,
-						},
-					},
-				},
-				{
-					Name:  "pipeline",
-					Usage: "Pipeline statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipeline indicator names",
-							Action: cli.RetrievePipelineIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve pipeline indicator value",
-							Action: cli.GetPipelineIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve pipeline indicator description",
-							Action: cli.GetPipelineIndicatorDesc,
-						},
-					},
-				},
-				{
-					Name:  "task",
-					Usage: "Task statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipeline indicator names",
-							Action: cli.RetrieveTaskIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve pipeline indicator value",
-							Action: cli.GetTaskIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve pipeline indicator description",
-							Action: cli.GetTaskIndicatorDesc,
-						},
-					},
-				},
-			},
-		},
-		{
-			Name:  "health",
-			Usage: "Health Interface",
-			Subcommands: []urfavecli.Command{
-				{
-					Name:   "check",
-					Usage:  "Check health of the gateway service instance",
-					Action: cli.Check,
-				},
-				{
-					Name:   "info",
-					Usage:  "Retrieve information of the gateway service instance",
-					Action: cli.Info,
-				},
-			},
-		},
-		{
-			Name:  "adminc",
-			Usage: "Cluster Administration Interface",
-			Flags: []urfavecli.Flag{
-				urfavecli.StringFlag{
-					Name:  "group",
-					Usage: "Indicates group name in cluster the request perform to",
-					Value: "default",
-				},
-				urfavecli.GenericFlag{
-					Name:  "timeout",
-					Usage: "Indicates request timeout limitation in senconds (10-65535)",
-					Value: common.NewUint16Value(30, nil),
-				},
-				urfavecli.BoolFlag{
-					Name: "consistent",
-					Usage: "Indicates request is performed in the group as " +
-						"consistency or availability first",
-				},
-			},
-			Subcommands: []urfavecli.Command{
-				{
-					Name:  "plugin",
-					Usage: "Plugin cluster administration interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "add",
-							// TODO: add -f --force to overwrite existing plugins
-							Usage:  "Create one or more plugins",
-							Action: cli.ClusterCreatePlugin,
-						},
-						{
-							Name:   "rm",
-							Usage:  "Delete one or more plugins",
-							Action: cli.ClusterDeletePlugin,
-						},
-						{
-							Name: "ls",
-							// TODO: add --only-name -n
-							// TODO: add --format (json, xml...)
-							// TODO: add --type -t
-							Usage:  "Retrieve plugins",
-							Action: cli.ClusterRetrievePlugins,
-						},
-						{
-							Name: "update",
-							// TODO: add -f --force to add plugins which does not exist
-							Usage:  "Update one or more plugins",
-							Action: cli.ClusterUpdatePlugin,
-						},
-						{
-							Name:   "types",
-							Usage:  "Retrieve plugin types",
-							Action: cli.ClusterRetrievePluginTypes,
-						},
-					},
-				},
-				{
-					Name:  "pipeline",
-					Usage: "Pipeline cluster administration interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "add",
-							// TODO: add -f --force to overwrite existing pipelines
-							Usage:  "Create one or more pipelines",
-							Action: cli.ClusterCreatePipeline,
-						},
-						{
-							Name:   "rm",
-							Usage:  "Delete one or more pipelines",
-							Action: cli.ClusterDeletePipeline,
-						},
-						{
-							Name: "ls",
-							// TODO: add -n --only-name
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipelines",
-							Action: cli.ClusterRetrievePipelines,
-						},
-						{
-							Name: "update",
-							// TODO: add -f --force to add pipeline which does not exist
-							Usage:  "Update one or more pipelines",
-							Action: cli.ClusterUpdatePipeline,
-						},
-						{
-							Name:   "types",
-							Usage:  "Retrieve pipeline types",
-							Action: cli.ClusterRetrievePipelineTypes,
-						},
-					},
-				},
-			},
-		},
-		{
-			Name:  "statc",
-			Usage: "Cluster Statistics Interface",
-			Flags: []urfavecli.Flag{
-				urfavecli.StringFlag{
-					Name:  "group",
-					Usage: "Indicates group name in cluster the request perform to",
-					Value: "default",
-				},
-				urfavecli.GenericFlag{
-					Name:  "timeout",
-					Usage: "Indicates request timeout limitation in senconds (10-65535)",
-					Value: common.NewUint16Value(30, nil),
-				},
-			},
-			Subcommands: []urfavecli.Command{
-				{
-					Name:  "plugin",
-					Usage: "Plugin cluster statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve plugin indicator names",
-							Action: cli.ClusterRetrievePluginIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve plugin indicator value",
-							Action: cli.ClusterGetPluginIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve plugin indicator description",
-							Action: cli.ClusterGetPluginIndicatorDesc,
-						},
-					},
-				},
-				{
-					Name:  "pipeline",
-					Usage: "Pipeline cluster statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipeline indicator names",
-							Action: cli.ClusterRetrievePipelineIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve pipeline indicator value",
-							Action: cli.ClusterGetPipelineIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve pipeline indicator description",
-							Action: cli.ClusterGetPipelineIndicatorDesc,
-						},
-					},
-				},
-				{
-					Name:  "task",
-					Usage: "Task cluster statistics interface",
-					Flags: []urfavecli.Flag{},
-					Subcommands: []urfavecli.Command{
-						{
-							Name: "ls",
-							// TODO: add --format (json, xml...)
-							Usage:  "Retrieve pipeline indicator names",
-							Action: cli.ClusterRetrieveTaskIndicatorNames,
-						},
-						{
-							Name:   "value",
-							Usage:  "Retrieve pipeline indicator value",
-							Action: cli.ClusterGetTaskIndicatorValue,
-						},
-						{
-							Name:   "desc",
-							Usage:  "Retrieve pipeline indicator description",
-							Action: cli.ClusterGetTaskIndicatorDesc,
-						},
-					},
-				},
-			},
-		},
+		adminCmd, statCmd, healthCmd,
+		admincCmd, statcCmd,
 	}
 
 	err := app.Run(os.Args)
@@ -420,4 +69,362 @@ func main() {
 		errStr := err.Error()
 		fmt.Println(errStr)
 	}
+}
+
+var adminCmd = urfavecli.Command{
+	Name:  "admin",
+	Usage: "Administration interface",
+	Subcommands: []urfavecli.Command{
+		{
+			Name:  "plugin",
+			Usage: "Plugin administration interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "add",
+					// TODO: add -f --force to overwrite existing plugins
+					Usage:  "Create one or more plugins",
+					Action: cli.CreatePlugin,
+				},
+				{
+					Name:   "rm",
+					Usage:  "Delete one or more plugins",
+					Action: cli.DeletePlugin,
+				},
+				{
+					Name: "ls",
+					// TODO: add --only-name -n
+					// TODO: add --format (json, xml...)
+					// TODO: add --type -t
+					Usage:  "Retrieve plugins",
+					Action: cli.RetrievePlugins,
+				},
+				{
+					Name: "update",
+					// TODO: add -f --force to add plugins which does not exist
+					Usage:  "Update one or more plugins",
+					Action: cli.UpdatePlugin,
+				},
+				{
+					Name:   "types",
+					Usage:  "Retrieve plugin types",
+					Action: cli.RetrievePluginTypes,
+				},
+			},
+		},
+		{
+			Name:  "pipeline",
+			Usage: "Pipeline administration interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "add",
+					// TODO: add -f --force to overwrite existing pipelines
+					Usage:  "Create one or more pipelines",
+					Action: cli.CreatePipeline,
+				},
+				{
+					Name:   "rm",
+					Usage:  "Delete one or more pipelines",
+					Action: cli.DeletePipeline,
+				},
+				{
+					Name: "ls",
+					// TODO: add -n --only-name
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipelines",
+					Action: cli.RetrievePipelines,
+				},
+				{
+					Name: "update",
+					// TODO: add -f --force to add pipeline which does not exist
+					Usage:  "Update one or more pipelines",
+					Action: cli.UpdatePipeline,
+				},
+				{
+					Name:   "types",
+					Usage:  "Retrieve pipeline types",
+					Action: cli.RetrievePipelineTypes,
+				},
+			},
+		},
+	},
+}
+
+var statCmd = urfavecli.Command{
+	Name:  "stat",
+	Usage: "Statistics interface",
+	Subcommands: []urfavecli.Command{
+		{
+			Name:  "plugin",
+			Usage: "Plugin statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve plugin indicator names",
+					Action: cli.RetrievePluginIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve plugin indicator value",
+					Action: cli.GetPluginIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve plugin indicator description",
+					Action: cli.GetPluginIndicatorDesc,
+				},
+			},
+		},
+		{
+			Name:  "pipeline",
+			Usage: "Pipeline statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipeline indicator names",
+					Action: cli.RetrievePipelineIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve pipeline indicator value",
+					Action: cli.GetPipelineIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve pipeline indicator description",
+					Action: cli.GetPipelineIndicatorDesc,
+				},
+			},
+		},
+		{
+			Name:  "task",
+			Usage: "Task statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipeline indicator names",
+					Action: cli.RetrieveTaskIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve pipeline indicator value",
+					Action: cli.GetTaskIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve pipeline indicator description",
+					Action: cli.GetTaskIndicatorDesc,
+				},
+			},
+		},
+	},
+}
+
+var healthCmd = urfavecli.Command{
+	Name:  "health",
+	Usage: "Health Interface",
+	Subcommands: []urfavecli.Command{
+		{
+			Name:   "check",
+			Usage:  "Check health of the gateway service instance",
+			Action: cli.Check,
+		},
+		{
+			Name:   "info",
+			Usage:  "Retrieve information of the gateway service instance",
+			Action: cli.Info,
+		},
+	},
+}
+
+var admincCmd = urfavecli.Command{
+	Name:  "adminc",
+	Usage: "Cluster Administration Interface",
+	Flags: []urfavecli.Flag{
+		urfavecli.StringFlag{
+			Name:  "group",
+			Usage: "Indicates group name in cluster the request perform to",
+			Value: "default",
+		},
+		urfavecli.GenericFlag{
+			Name:  "timeout",
+			Usage: "Indicates request timeout limitation in senconds (10-65535)",
+			Value: common.NewUint16Value(30, nil),
+		},
+		urfavecli.BoolFlag{
+			Name: "consistent",
+			Usage: "Indicates request is performed in the group as " +
+				"consistency or availability first",
+		},
+	},
+	Subcommands: []urfavecli.Command{
+		{
+			Name:  "plugin",
+			Usage: "Plugin cluster administration interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "add",
+					// TODO: add -f --force to overwrite existing plugins
+					Usage:  "Create one or more plugins",
+					Action: cli.ClusterCreatePlugin,
+				},
+				{
+					Name:   "rm",
+					Usage:  "Delete one or more plugins",
+					Action: cli.ClusterDeletePlugin,
+				},
+				{
+					Name: "ls",
+					// TODO: add --only-name -n
+					// TODO: add --format (json, xml...)
+					// TODO: add --type -t
+					Usage:  "Retrieve plugins",
+					Action: cli.ClusterRetrievePlugins,
+				},
+				{
+					Name: "update",
+					// TODO: add -f --force to add plugins which does not exist
+					Usage:  "Update one or more plugins",
+					Action: cli.ClusterUpdatePlugin,
+				},
+				{
+					Name:   "types",
+					Usage:  "Retrieve plugin types",
+					Action: cli.ClusterRetrievePluginTypes,
+				},
+			},
+		},
+		{
+			Name:  "pipeline",
+			Usage: "Pipeline cluster administration interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "add",
+					// TODO: add -f --force to overwrite existing pipelines
+					Usage:  "Create one or more pipelines",
+					Action: cli.ClusterCreatePipeline,
+				},
+				{
+					Name:   "rm",
+					Usage:  "Delete one or more pipelines",
+					Action: cli.ClusterDeletePipeline,
+				},
+				{
+					Name: "ls",
+					// TODO: add -n --only-name
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipelines",
+					Action: cli.ClusterRetrievePipelines,
+				},
+				{
+					Name: "update",
+					// TODO: add -f --force to add pipeline which does not exist
+					Usage:  "Update one or more pipelines",
+					Action: cli.ClusterUpdatePipeline,
+				},
+				{
+					Name:   "types",
+					Usage:  "Retrieve pipeline types",
+					Action: cli.ClusterRetrievePipelineTypes,
+				},
+			},
+		},
+	},
+}
+
+var statcCmd = urfavecli.Command{
+	Name:  "statc",
+	Usage: "Cluster Statistics Interface",
+	Flags: []urfavecli.Flag{
+		urfavecli.StringFlag{
+			Name:  "group",
+			Usage: "Indicates group name in cluster the request perform to",
+			Value: "default",
+		},
+		urfavecli.GenericFlag{
+			Name:  "timeout",
+			Usage: "Indicates request timeout limitation in senconds (10-65535)",
+			Value: common.NewUint16Value(30, nil),
+		},
+	},
+	Subcommands: []urfavecli.Command{
+		{
+			Name:  "plugin",
+			Usage: "Plugin cluster statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve plugin indicator names",
+					Action: cli.ClusterRetrievePluginIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve plugin indicator value",
+					Action: cli.ClusterGetPluginIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve plugin indicator description",
+					Action: cli.ClusterGetPluginIndicatorDesc,
+				},
+			},
+		},
+		{
+			Name:  "pipeline",
+			Usage: "Pipeline cluster statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipeline indicator names",
+					Action: cli.ClusterRetrievePipelineIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve pipeline indicator value",
+					Action: cli.ClusterGetPipelineIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve pipeline indicator description",
+					Action: cli.ClusterGetPipelineIndicatorDesc,
+				},
+			},
+		},
+		{
+			Name:  "task",
+			Usage: "Task cluster statistics interface",
+			Flags: []urfavecli.Flag{},
+			Subcommands: []urfavecli.Command{
+				{
+					Name: "ls",
+					// TODO: add --format (json, xml...)
+					Usage:  "Retrieve pipeline indicator names",
+					Action: cli.ClusterRetrieveTaskIndicatorNames,
+				},
+				{
+					Name:   "value",
+					Usage:  "Retrieve pipeline indicator value",
+					Action: cli.ClusterGetTaskIndicatorValue,
+				},
+				{
+					Name:   "desc",
+					Usage:  "Retrieve pipeline indicator description",
+					Action: cli.ClusterGetTaskIndicatorDesc,
+				},
+			},
+		},
+	},
 }
