efd6b071d162854056fa99a119d6de73fd2b7c22
Zhi Yan Liu <lzy.dev@gmail.com>
2017-08-25 12:31:41 +0800

    [image] Update docker files of server on alpine and ubuntu distributions
    To leverage `-plugin_shell_root_namespace` option for shell plugin.

--- rootfs/ubuntu/entrypoint.server.sh
+++ rootfs/ubuntu/entrypoint.server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 0 ] ; then
-  set -- "-plugin_python_root_namespace"
+  set -- "-plugin_python_root_namespace" "-plugin_shell_root_namespace"
 fi
 
 if [ "$(echo $1 | head -c 1)" != "-" ] ; then
