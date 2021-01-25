802a7e43dcd48619d24ad504fd12a6270ad96add
Bomin Zhang <zhangbomin@megaease.com>
2021-01-25 18:00:23 +0800

    speed up stringtool.Cat

--- pkg/util/stringtool/stringtool.go
+++ pkg/util/stringtool/stringtool.go
@@ -6,7 +6,13 @@ import "strings"
 // It is intended to used in the core executing path for performance optimization.
 // fmt.Printf is still recommended for readability.
 func Cat(strs ...string) string {
+	n := 0
+	for _, s := range strs {
+		n += len(s)
+	}
+
 	var builder strings.Builder
+	builder.Grow(n)
 	for _, s := range strs {
 		builder.WriteString(s)
 	}
