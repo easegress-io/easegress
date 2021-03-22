c8e7744a79b04336cd6be7a436db195364bd2683
zouyingjie <yingjiezou1@gmail.com>
2021-03-22 11:12:31 +0800

    Add updateService operation for observability

--- pkg/object/meshcontroller/worker/observability.go
+++ pkg/object/meshcontroller/worker/observability.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	easeAgentConfigManager = "com.megaease.easeagent:type=ConfigManager"
+	updateServiceOperation = "updateService"
 )
 
 type (
@@ -37,9 +38,25 @@ func (server *ObservabilityManager) UpdateObservability(serviceName string, newO
 
 	logger.Infof("Update Service: %s Observability, new Observability is %s", serviceName, newObservability)
 
-	_, err := server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, "updateObservability", args)
+	_, err := server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, updateServiceOperation, args)
 	if err != nil {
-		return fmt.Errorf("updateObservability service: %s observability failed: %v", serviceName, err)
+		return fmt.Errorf("update service: %s observability failed: %v", serviceName, err)
+	}
+
+	return nil
+}
+
+
+// UpdateService updates service.
+func (server *ObservabilityManager) UpdateService(newService *spec.Service) error {
+	paramsMap := structs.Map(newService)
+	args := []interface{}{paramsMap}
+
+	logger.Infof("Update Service: %s Observability, new Service is %s", newService.Name, newService)
+
+	_, err := server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, updateServiceOperation, args)
+	if err != nil {
+		return fmt.Errorf("UpdateService service: %s  failed: %v", newService.Name, err)
 	}
 
 	return nil
