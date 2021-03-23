63b5988aace0c051d12242e3165adaff53997f7e
zouyingjie <yingjiezou1@gmail.com>
2021-03-23 12:08:29 +0800

    modify observability updateService processing

--- pkg/object/meshcontroller/worker/observability.go
+++ pkg/object/meshcontroller/worker/observability.go
@@ -1,7 +1,8 @@
-package worker
+package main
 
 import (
 	"fmt"
+	yaml "gopkg.in/yaml.v2"
 
 	"github.com/fatih/structs"
 	"github.com/megaease/easegateway/pkg/logger"
@@ -31,30 +32,24 @@ func NewObservabilityServer(serviceName string) *ObservabilityManager {
 	}
 }
 
-// UpdateObservability updates observability.
-func (server *ObservabilityManager) UpdateObservability(serviceName string, newObservability *spec.Observability) error {
-	paramsMap := structs.Map(newObservability)
-	args := []interface{}{paramsMap}
-
-	logger.Infof("Update Service: %s Observability, new Observability is %s", serviceName, newObservability)
-
-	_, err := server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, updateServiceOperation, args)
-	if err != nil {
-		return fmt.Errorf("update service: %s observability failed: %v", serviceName, err)
-	}
-
-	return nil
-}
-
-
 // UpdateService updates service.
 func (server *ObservabilityManager) UpdateService(newService *spec.Service, version int64) error {
-	paramsMap := structs.Map(newService)
-	args := []interface{}{paramsMap, version}
+
+	buff, err := yaml.Marshal(newService)
+	if err != nil {
+		panic(fmt.Errorf("marsharl new Service %#v failed: %v", err))
+	}
+	m := make(map[string]string)
+	err = yaml.Unmarshal(buff, m)
+	if err != nil {
+		panic(fmt.Errorf("marsharl new Service %#v failed: %v", err))
+	}
+
+	args := []interface{}{m, version}
 
 	logger.Infof("Update Service: %s Observability, new Service is %s", newService.Name, newService)
 
-	_, err := server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, updateServiceOperation, args)
+	_, err = server.jolokiaClient.ExecuteMbeanOperation(easeAgentConfigManager, updateServiceOperation, args)
 	if err != nil {
 		return fmt.Errorf("UpdateService service: %s  failed: %v", newService.Name, err)
 	}
