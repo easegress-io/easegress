e82bf03633d76051eb060e1833b65f1550b0f68c
wuyiheng <wuyiheng@megaease.com>
2021-03-29 21:21:16 +0800

    [canary] add more unit test

--- pkg/object/meshcontroller/spec/spec_test.go
+++ pkg/object/meshcontroller/spec/spec_test.go
@@ -128,9 +128,9 @@ func TestEgressPipelineWithCanarySpec(t *testing.T) {
 	fmt.Println(superSpec.YAMLConfig())
 }
 
-func TestEgressPipelineWithCanaryNoInstanceSpec(t *testing.T) {
+func TestEgressPipelineWithMultipleCanarySpec(t *testing.T) {
 	s := &Service{
-		Name: "order-002-canary",
+		Name: "order-002-canary-array",
 		LoadBalance: &LoadBalance{
 			Policy: backend.PolicyIPHash,
 		},
@@ -148,6 +148,83 @@ func TestEgressPipelineWithCanaryNoInstanceSpec(t *testing.T) {
 						Headers: map[string]*httpfilter.ValueFilter{
 							"X-canary": &httpfilter.ValueFilter{
 								Values: []string{"v1"},
+							},
+						},
+					},
+					ServiceLabels: map[string]string{
+						"version": "v1",
+					},
+				},
+				{
+					Filter: &httpfilter.Spec{
+						Headers: map[string]*httpfilter.ValueFilter{
+							"X-canary": &httpfilter.ValueFilter{
+								Values: []string{"ams"},
+							},
+						},
+					},
+					ServiceLabels: map[string]string{
+						"version": "v2",
+					},
+				},
+			},
+		},
+	}
+
+	instanceSpecs := []*ServiceInstanceSpec{
+		{
+			ServiceName: "fake-001",
+			InstanceID:  "xxx-89757",
+			IP:          "192.168.0.110",
+			Port:        80,
+			Status:      "UP",
+		},
+		{
+			ServiceName: "fake-002-canary",
+			InstanceID:  "zzz-73597",
+			IP:          "192.168.0.120",
+			Port:        80,
+			Status:      "UP",
+			Labels: map[string]string{
+				"version": "v1",
+			},
+		},
+		{
+			ServiceName: "fake-003-canary-no-match",
+			InstanceID:  "yyy-73587",
+			IP:          "192.168.0.121",
+			Port:        80,
+			Status:      "UP",
+			Labels: map[string]string{
+				"version": "v2",
+			},
+		},
+	}
+
+	superSpec := s.EgressPipelineSpec(instanceSpecs)
+	fmt.Println(superSpec.YAMLConfig())
+}
+
+func TestEgressPipelineWithCanaryNoInstanceSpec(t *testing.T) {
+	s := &Service{
+		Name: "order-002-canary-no-instance",
+		LoadBalance: &LoadBalance{
+			Policy: backend.PolicyIPHash,
+		},
+		Sidecar: &Sidecar{
+			Address:         "127.0.0.1",
+			IngressPort:     8080,
+			IngressProtocol: "http",
+			EgressPort:      9090,
+			EgressProtocol:  "http",
+		},
+		Canary: &Canary{
+			CanaryRules: []*CanaryRule{
+				{
+					Filter: &httpfilter.Spec{
+						Headers: map[string]*httpfilter.ValueFilter{
+							"X-canary": &httpfilter.ValueFilter{
+								Values: []string{"aaa"},
 							},
 						},
 					},
