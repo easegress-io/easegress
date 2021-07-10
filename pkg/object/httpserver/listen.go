6d63a22a4d1fa9e1ebc9b65f0d1db0be8dbd8fa5
Bomin Zhang <zhangbomin@megaease.com>
2021-07-10 12:30:55 +0800

    fix bug #114 (#115)
    * fix bug #114
    
    * add comment to `SetMaxCount`
    
    Co-authored-by: Bomin Zhang <bomin.zhang@megaease.com>

--- pkg/object/httpserver/listen.go
+++ pkg/object/httpserver/listen.go
@@ -18,6 +18,7 @@
 package httpserver
 
 import (
+	"context"
 	"net"
 	"sync"
 
@@ -27,10 +28,13 @@ import (
 // NewLimitListener returns a Listener that accepts at most n simultaneous
 // connections from the provided Listener.
 func NewLimitListener(l net.Listener, n uint32) *LimitListener {
+	ctx, cancel := context.WithCancel(context.Background())
+
 	return &LimitListener{
 		Listener: l,
 		sem:      sem2.NewSem(n),
-		done:     make(chan struct{}),
+		ctx:      ctx,
+		cancel:   cancel,
 	}
 }
 
@@ -38,33 +42,35 @@ func NewLimitListener(l net.Listener, n uint32) *LimitListener {
 type LimitListener struct {
 	net.Listener
 	sem       *sem2.Semaphore
-	closeOnce sync.Once     // ensures the done chan is only closed once
-	done      chan struct{} // no values sent; closed when Close is called
+	ctx       context.Context
+	cancel    context.CancelFunc
+	closeOnce sync.Once // ensures the done chan is only closed once
 }
 
 // acquire acquires the limiting semaphore. Returns true if successfully
 // accquired, false if the listener is closed and the semaphore is not
 // acquired.
 func (l *LimitListener) acquire() bool {
-	select {
-	case <-l.done:
-		return false
-	case <-l.sem.AcquireRaw():
-		return true
-	}
+	return l.sem.AcquireWithContext(l.ctx) == nil
 }
-func (l *LimitListener) release() { l.sem.Release() }
+
+func (l *LimitListener) release() {
+	l.sem.Release()
+}
 
 // Accept accepts one connection.
 func (l *LimitListener) Accept() (net.Conn, error) {
 	acquired := l.acquire()
-	// If the semaphore isn't acquired because the listener was closed, expect
-	// that this call to accept won't block, but immediately return an error.
-	c, err := l.Listener.Accept()
-	if err != nil {
+	if err := l.ctx.Err(); err != nil {
 		if acquired {
 			l.release()
 		}
+		return nil, err
+	}
+
+	c, err := l.Listener.Accept()
+	if err != nil {
+		l.release()
 		return nil, err
 	}
 	return &limitListenerConn{Conn: c, release: l.release}, nil
@@ -78,7 +84,7 @@ func (l *LimitListener) SetMaxConnection(n uint32) {
 // Close closes LimitListener.
 func (l *LimitListener) Close() error {
 	err := l.Listener.Close()
-	l.closeOnce.Do(func() { close(l.done) })
+	l.closeOnce.Do(l.cancel)
 	return err
 }
 
