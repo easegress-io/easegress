4d5baa40cc5d7633c4502c9022cca8531973de6e
chen <chen.su1994@outlook.com>
2021-11-11 11:36:21 +0800

    update mqtt filter ConnectControl

--- pkg/filter/connectcontrol/connectcontrol.go
+++ pkg/filter/connectcontrol/connectcontrol.go
@@ -18,14 +18,12 @@
 package connectcontrol
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
-	"net/http"
+	"regexp"
 	"sync"
 
-	"github.com/megaease/easegress/pkg/api"
 	"github.com/megaease/easegress/pkg/context"
+	"github.com/megaease/easegress/pkg/logger"
 	"github.com/megaease/easegress/pkg/object/pipeline"
 )
 
@@ -33,7 +31,6 @@ const (
 	// Kind is the kind of ConnectControl
 	Kind = "ConnectControl"
 
-	resultWrongPacket         = "wrongPacketError"
 	resultBannedClientOrTopic = "bannedClientOrTopicError"
 )
 
@@ -46,27 +43,30 @@ type (
 	// if MQTTContext ClientID in bannedClients, the connection will be closed,
 	// if MQTTContext publish topic in bannedTopics, the connection will be closed.
 	ConnectControl struct {
-		filterSpec    *pipeline.FilterSpec
-		spec          *Spec
-		bannedClients sync.Map
-		bannedTopics  sync.Map
+		filterSpec     *pipeline.FilterSpec
+		spec           *Spec
+		bannedClients  sync.Map
+		bannedTopics   sync.Map
+		bannedClientRe *regexp.Regexp
+		bannedTopicRe  *regexp.Regexp
+		status         *Status
 	}
 
 	// Spec describes the ConnectControl
 	Spec struct {
-		BannedClients []string `yaml:"bannedClients" jsonschema:"omitempty"`
-		BannedTopics  []string `yaml:"bannedTopics" jsonschema:"omitempty"`
-		EarlyStop     bool     `yaml:"earlyStop" jsonschema:"omitempty"`
+		BannedClientRe string   `yaml:"bannedClientRe" jsonschema:"omitempty"`
+		BannedClients  []string `yaml:"bannedClients" jsonschema:"omitempty"`
+		BannedTopicRe  string   `yaml:"bannedTopicRe" jsonschema:"omitempty"`
+		BannedTopics   []string `yaml:"bannedTopics" jsonschema:"omitempty"`
+		EarlyStop      bool     `yaml:"earlyStop" jsonschema:"omitempty"`
 	}
 
 	// Status is ConnectControl filter status
 	Status struct {
-	}
-
-	// HTTPJsonData is json data struct for ConnectControl filter to process http request
-	HTTPJsonData struct {
-		Clients []string `yaml:"clients" jsonschema:"omitempty"`
-		Topics  []string `yaml:"topics" jsonschema:"omitempty"`
+		BannedClientRe string   `yaml:"bannedClientRe" jsonschema:"omitempty"`
+		BannedClients  []string `yaml:"bannedClients" jsonschema:"omitempty"`
+		BannedTopicRe  string   `yaml:"bannedTopicRe" jsonschema:"omitempty"`
+		BannedTopics   []string `yaml:"bannedTopics" jsonschema:"omitempty"`
 	}
 )
 
@@ -103,11 +103,35 @@ func (cc *ConnectControl) Init(filterSpec *pipeline.FilterSpec) {
 }
 
 func (cc *ConnectControl) reload() {
+	if len(cc.spec.BannedClientRe) > 0 {
+		r, err := regexp.Compile(cc.spec.BannedClientRe)
+		if err != nil {
+			logger.Errorf("filter ConnectControl compile BannedClientRe %s failed, %s", cc.spec.BannedClientRe, err)
+		} else {
+			cc.bannedClientRe = r
+		}
+	}
+	if len(cc.spec.BannedTopicRe) > 0 {
+		r, err := regexp.Compile(cc.spec.BannedTopicRe)
+		if err != nil {
+			logger.Errorf("filter ConnectControl compile BannedTopicRe %s failed, %s", cc.spec.BannedTopicRe, err)
+		} else {
+			cc.bannedTopicRe = r
+		}
+	}
+
 	for _, c := range cc.spec.BannedClients {
 		cc.bannedClients.Store(c, struct{}{})
 	}
 	for _, t := range cc.spec.BannedTopics {
 		cc.bannedTopics.Store(t, struct{}{})
+	}
+
+	cc.status = &Status{
+		BannedClientRe: cc.spec.BannedClientRe,
+		BannedTopicRe:  cc.spec.BannedTopicRe,
+		BannedClients:  cc.spec.BannedClients,
+		BannedTopics:   cc.spec.BannedTopics,
 	}
 }
 
@@ -119,109 +143,43 @@ func (cc *ConnectControl) Inherit(filterSpec *pipeline.FilterSpec, previousGener
 
 // Status return status of ConnectControl
 func (cc *ConnectControl) Status() interface{} {
-	return nil
+	return cc.status
 }
 
 // Close close ConnectControl gracefully
 func (cc *ConnectControl) Close() {
 }
 
+func (cc *ConnectControl) checkBan(ctx context.MQTTContext) bool {
+	cid := ctx.Client().ClientID()
+	if cc.bannedClientRe != nil && cc.bannedClientRe.MatchString(cid) {
+		return true
+	}
+	if _, ok := cc.bannedClients.Load(cid); ok {
+		return true
+	}
+	topic := ctx.PublishPacket().TopicName
+	if cc.bannedTopicRe != nil && cc.bannedTopicRe.MatchString(topic) {
+		return true
+	}
+	if _, ok := cc.bannedTopics.Load(topic); ok {
+		return true
+	}
+	return false
+}
+
 // HandleMQTT handle MQTT request
 func (cc *ConnectControl) HandleMQTT(ctx context.MQTTContext) *context.MQTTResult {
 	if ctx.PacketType() != context.MQTTPublish {
-		return &context.MQTTResult{Err: errors.New(resultWrongPacket)}
+		return &context.MQTTResult{}
 	}
 
-	cid := ctx.Client().ClientID()
-	if _, ok := cc.bannedClients.Load(cid); !ok {
-		topic := ctx.PublishPacket().TopicName
-		if _, ok := cc.bannedTopics.Load(topic); !ok {
-			return &context.MQTTResult{}
+	if cc.checkBan(ctx) {
+		ctx.SetDisconnect()
+		if cc.spec.EarlyStop {
+			ctx.SetEarlyStop()
 		}
+		return &context.MQTTResult{Err: errors.New(resultBannedClientOrTopic)}
 	}
-
-	ctx.SetDisconnect()
-	if cc.spec.EarlyStop {
-		ctx.SetEarlyStop()
-	}
-	return &context.MQTTResult{Err: errors.New(resultBannedClientOrTopic)}
-}
-
-// APIs return apis for ConnectControl filter
-func (cc *ConnectControl) APIs() []*pipeline.APIEntry {
-	addName := func(path string) string {
-		return fmt.Sprintf(path, cc.filterSpec.Name())
-	}
-	entries := []*pipeline.APIEntry{
-		{Path: addName("connectcontrol/%s/banclient"), Method: http.MethodPost, Handler: cc.handleBanClient},
-		{Path: addName("connectcontrol/%s/unbanclient"), Method: http.MethodPost, Handler: cc.handleUnbanClient},
-		{Path: addName("connectcontrol/%s/info"), Method: http.MethodGet, Handler: cc.handleInfo},
-	}
-	return entries
-}
-
-func (cc *ConnectControl) handleBanClient(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		api.HandleAPIError(w, r, http.StatusBadRequest, fmt.Errorf("suppose POST request but got %s", r.Method))
-		return
-	}
-	var data HTTPJsonData
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		api.HandleAPIError(w, r, http.StatusBadRequest, fmt.Errorf("invalid json data from request body"))
-		return
-	}
-	for _, c := range data.Clients {
-		cc.bannedClients.Store(c, struct{}{})
-	}
-	for _, t := range data.Topics {
-		cc.bannedTopics.Store(t, struct{}{})
-	}
-}
-
-func (cc *ConnectControl) handleUnbanClient(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		api.HandleAPIError(w, r, http.StatusBadRequest, fmt.Errorf("suppose POST request but got %s", r.Method))
-		return
-	}
-	var data HTTPJsonData
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		api.HandleAPIError(w, r, http.StatusBadRequest, fmt.Errorf("invalid json data from request body"))
-		return
-	}
-	for _, c := range data.Clients {
-		cc.bannedClients.Delete(c)
-	}
-	for _, t := range data.Topics {
-		cc.bannedTopics.Delete(t)
-	}
-}
-
-func (cc *ConnectControl) handleInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		api.HandleAPIError(w, r, http.StatusBadRequest, fmt.Errorf("suppose POST request but got %s", r.Method))
-		return
-	}
-	clients := []string{}
-	cc.bannedClients.Range(func(key, value interface{}) bool {
-		clients = append(clients, key.(string))
-		return true
-	})
-
-	topics := []string{}
-	cc.bannedTopics.Range(func(key, value interface{}) bool {
-		topics = append(topics, key.(string))
-		return true
-	})
-
-	data := HTTPJsonData{
-		Clients: clients,
-		Topics:  topics,
-	}
-	err := json.NewEncoder(w).Encode(&data)
-	if err != nil {
-		api.HandleAPIError(w, r, http.StatusInternalServerError, fmt.Errorf("encode json data failed"))
-		return
-	}
+	return &context.MQTTResult{}
 }
