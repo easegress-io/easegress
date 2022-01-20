aefab34d17cb0f3db01fc6eccf6ca2c85ee7bc37
Samu Tamminen <samu@megaease.com>
2022-01-20 17:15:06 +0800

    test challenges and MockFilterSpec

--- pkg/object/autocertmanager/autocertmanager_test.go
+++ pkg/object/autocertmanager/autocertmanager_test.go
@@ -829,6 +829,13 @@ func waitDNSRecordTest(t *testing.T, d Domain) {
 }
 
 func TestDomain(t *testing.T) {
+	etcdDirName, err := ioutil.TempDir("", "autocertmanager-domain-test")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer os.RemoveAll(etcdDirName)
+	cls := cluster.CreateClusterForTest(etcdDirName)
+
 	t.Run("waitDNSRecord", func(t *testing.T) {
 		d := Domain{}
 		waitDNSRecordTest(t, d)
@@ -862,5 +869,42 @@ func TestDomain(t *testing.T) {
 			t.Errorf("should have failed")
 		}
 		ca.Close()
+	})
+
+	t.Run("runHTTP01",func(t *testing.T) {
+		fakeAcm := &AutoCertManager{}
+		fakeAcm.stopCtx, fakeAcm.cancel = context.WithCancel(context.Background())
+		key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		fakeAcm.client = &acme.Client{Key: key, DirectoryURL: ""}
+		chal := &acme.Challenge{Token: "asdoilj"}
+		fakeAcm.storage = &storage{cls: cls}
+		// let's close the cluster to make runHTTP01 fail
+		closeWG := &sync.WaitGroup{}
+		closeWG.Add(1)
+		cls.CloseServer(closeWG)
+		closeWG.Wait()
+		d := Domain{nameInPunyCode: "name"}
+		if err := d.runHTTP01(fakeAcm,chal); err == nil {
+			t.Errorf("should have failed")
+		}
+	})
+
+	t.Run("runTLSALPN01",func(t *testing.T) {
+		fakeAcm := &AutoCertManager{}
+		fakeAcm.stopCtx, fakeAcm.cancel = context.WithCancel(context.Background())
+		key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		fakeAcm.client = &acme.Client{Key: key, DirectoryURL: ""}
+		chal := &acme.Challenge{Token: "asdoilj"}
+		auth := &acme.Authorization{Identifier:acme.AuthzID{Value: "authzID"}}
+		fakeAcm.storage = &storage{cls: cls}
+		// let's close the cluster to make runTLSALPN01 fail
+		closeWG := &sync.WaitGroup{}
+		closeWG.Add(1)
+		cls.CloseServer(closeWG)
+		closeWG.Wait()
+		d := Domain{nameInPunyCode: "name"}
+		if err := d.runTLSALPN01(fakeAcm, auth, chal); err == nil {
+			t.Errorf("should have failed")
+		}
 	})
 }
