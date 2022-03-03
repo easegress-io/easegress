8649d0043063212e860e5c30c999917e6cc16811
Bomin Zhang <zhangbomin@megaease.com>
2022-03-03 14:44:20 +0800

    filter out 'TLS handshake error' (close #481) (#533)

---
+++ pkg/util/filterwriter/filterwriter.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package filterwriter
+
+import (
+	"io"
+)
+
+// FilterFunc is a filter function for the Filter Writer,
+// returns true to write 'p' to the underlying writer,
+// return false to discard.
+type FilterFunc func(p []byte) bool
+
+type filterWriter struct {
+	filter FilterFunc
+	w      io.Writer
+}
+
+// New creates a Filter Writer
+func New(w io.Writer, filter FilterFunc) io.Writer {
+	return &filterWriter{
+		filter: filter,
+		w:      w,
+	}
+}
+
+// Write implements io.Writer
+func (fw *filterWriter) Write(p []byte) (int, error) {
+	if fw.filter(p) {
+		return fw.w.Write(p)
+	}
+	return len(p), nil
+}
---
+++ pkg/util/filterwriter/filterwriter_test.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package filterwriter
+
+import (
+	"bytes"
+	"testing"
+)
+
+type counterWriter struct {
+	count int
+}
+
+func (cw *counterWriter) Write(p []byte) (int, error) {
+	cw.count++
+	return len(p), nil
+}
+
+func TestFilterWriter(t *testing.T) {
+	cw := &counterWriter{}
+
+	fw := New(cw, func(p []byte) bool {
+		return !bytes.Contains(p, []byte("filter out"))
+	})
+
+	fw.Write([]byte("abcd"))
+	if cw.count != 1 {
+		t.Error("data should be filtered in.")
+	}
+
+	fw.Write([]byte("abcd filter out"))
+	if cw.count > 1 {
+		t.Error("data should be filtered out.")
+	}
+}
