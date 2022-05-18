a8678ea19e69fbb599dc311b56475b3f37aed2cc
chen <suchen@megaease.com>
2022-05-18 14:50:29 +0800

    add more test

--- pkg/protocols/httpprot/response_test.go
+++ pkg/protocols/httpprot/response_test.go
@@ -18,16 +18,78 @@
 package httpprot
 
 import (
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNilResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	resp, err := NewResponse(nil)
+	assert.Nil(err)
+	defer resp.Close()
+	resp.HTTPHeader().Set("foo", "bar")
+
+	l, err := resp.FetchPayload()
+	assert.Nil(err)
+	assert.Equal(0, l)
+
+	resp.SetPayload([]byte("hello"))
+	r := resp.GetPayload()
+	data, err := io.ReadAll(r)
+	assert.Nil(err)
+	assert.Equal([]byte("hello"), data)
+	assert.Equal([]byte("hello"), resp.RawPayload())
+
+	assert.NotNil(resp.Std())
+	assert.NotZero(resp.MetaSize())
+	assert.Equal(http.StatusOK, resp.StatusCode())
+
+	resp.SetStatusCode(http.StatusBadRequest)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode())
+
+	cookie := &http.Cookie{
+		Name:   "key",
+		Value:  "value",
+		MaxAge: 300,
+	}
+	resp.SetCookie(cookie)
+	assert.Equal(cookie.Name, resp.Cookies()[0].Name)
+}
+
 func TestResponse(t *testing.T) {
 	assert := assert.New(t)
 
-	resp, _ := NewResponse(nil)
-	resp.SetStatusCode(http.StatusCreated)
-	assert.Equal(http.StatusCreated, resp.StatusCode())
+	rw := httptest.NewRecorder()
+	l, err := rw.Write([]byte("hello"))
+	assert.Nil(err)
+	assert.Equal(5, l)
+
+	r := rw.Result()
+	resp, err := NewResponse(r)
+	assert.Nil(err)
+	defer resp.Close()
+	resp.HTTPHeader().Set("foo", "bar")
+
+	l, err = resp.FetchPayload()
+	assert.Nil(err)
+	assert.Equal(5, l)
+
+	resp.SetPayload([]byte("new"))
+	reader := resp.GetPayload()
+	data, err := io.ReadAll(reader)
+	assert.Nil(err)
+	assert.Equal([]byte("new"), data)
+	assert.Equal([]byte("new"), resp.RawPayload())
+
+	assert.NotNil(resp.Std())
+	assert.NotZero(resp.MetaSize())
+	assert.Equal(http.StatusOK, resp.StatusCode())
+
+	resp.SetStatusCode(http.StatusBadRequest)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode())
 }
