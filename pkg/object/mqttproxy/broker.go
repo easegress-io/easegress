af09b923534619ee33ae4ae811a4737990efd8da
chen <suchen@megaease.com>
2022-08-02 14:23:56 +0800

    update mqtt close conn strategy

--- pkg/object/mqttproxy/broker.go
+++ pkg/object/mqttproxy/broker.go
@@ -384,7 +384,8 @@ func (b *Broker) handleConn(conn net.Conn) {
 		}
 	}
 	go client.writeLoop()
-	client.readLoop()
+	go client.readLoop()
+	<-client.done
 }
 
 func (b *Broker) setSession(client *Client, connect *packets.ConnectPacket) {
