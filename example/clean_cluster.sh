f7f74c5b4108ba8bb7a301a18e10148b0360f898
hanjuntao <jthann@qq.com>
2022-08-02 16:15:29 +0800

    add files to .gitignore and to example clean scripts (#742)

--- example/clean_cluster.sh
+++ example/clean_cluster.sh
@@ -8,5 +8,5 @@ SCRIPTFILE=`basename $0`
 for MEMBER_PATH in primary-00{1,2,3} secondary-00{4,5} primary-single
 do
 	${SCRIPTPATH}/${MEMBER_PATH}/stop.sh -f
-	rm -fr ${SCRIPTPATH}/${MEMBER_PATH}/{nohup.out,log,member,data,data_bak,easegress.pid,running_objects.yaml}
+	rm -fr ${SCRIPTPATH}/${MEMBER_PATH}/{nohup.out,log,member,data,data_bak,easegress.pid,running_objects.yaml,nacos*}
 done
