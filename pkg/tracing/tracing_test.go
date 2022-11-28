6a6e20e7414c66541a4c01698381cd8023dc3cb0
aniaan <fjn.aniaan@gmail.com>
2022-11-29 00:59:31 +0800

    fix(license): add license

--- pkg/tracing/tracing_test.go
+++ pkg/tracing/tracing_test.go
@@ -1,11 +1,29 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tracing
 
 import (
+	"strings"
+	"testing"
+
 	"github.com/megaease/easegress/pkg/logger"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel/sdk/trace"
-	"strings"
-	"testing"
 )
 
 func init() {
@@ -54,7 +72,6 @@ func TestNewResource(t *testing.T) {
 
 	assert.Nil(err)
 	assert.True(strings.Contains(r.String(), "k=v,service.name=test"))
-
 }
 
 func TestNewSampler(t *testing.T) {
@@ -77,5 +94,4 @@ func TestNewSampler(t *testing.T) {
 	}
 	s = spec.newSampler()
 	assert.Equal(trace.TraceIDRatioBased(0.5), s)
-
 }
