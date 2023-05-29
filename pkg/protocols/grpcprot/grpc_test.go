b2982fe9421b55d6acb81498d74b282800831c1c
Bomin Zhang <zhangbomin@megaease.com>
2023-05-29 18:10:20 +0800

    improve coverage for grpcprot

---
+++ pkg/protocols/grpcprot/grpc_test.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grpcprot
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProtocol(t *testing.T) {
+	assert := assert.New(t)
+
+	p := &Protocol{}
+
+	_, err := p.CreateRequest(nil)
+	assert.Error(err)
+
+	_, err = p.CreateRequest(NewFakeServerStream(context.Background()))
+	assert.NoError(err)
+
+	_, err = p.CreateResponse(nil)
+	assert.NoError(err)
+
+	assert.Panics(func() { p.NewRequestInfo() })
+	assert.Panics(func() { p.BuildRequest(nil) })
+	assert.Panics(func() { p.NewResponseInfo() })
+	assert.Panics(func() { p.BuildResponse(nil) })
+}
--- pkg/protocols/grpcprot/header_test.go
+++ pkg/protocols/grpcprot/header_test.go
@@ -18,9 +18,10 @@
 package grpcprot
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/metadata"
-	"testing"
 )
 
 func TestValueNotString(t *testing.T) {
@@ -38,5 +39,130 @@ func TestValueNotString(t *testing.T) {
 		assertion.Panics(func() {
 			header.Set("test", arr)
 		})
+	})
+}
+
+func TestNewTrailer(t *testing.T) {
+	assertion := assert.New(t)
+	md := metadata.New(nil)
+	trailer := NewTrailer(md)
+	assertion.NotNil(trailer)
+}
+
+func TestAdd(t *testing.T) {
+	assertion := assert.New(t)
+
+	t.Run("append single value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.Add("test", "test")
+		assertion.Equal([]string{"test"}, header.Get("test"))
+	})
+
+	t.Run("append multiple value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.Add("test", []string{"test1", "test2"})
+		assertion.Equal([]string{"test1", "test2"}, header.Get("test"))
+	})
+
+	t.Run("append integer value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		assertion.Panics(func() {
+			header.Add("test", 10)
+		})
+	})
+
+	t.Run("raw add", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.RawAdd("test", "test1", "test2")
+		assertion.Equal([]string{"test1", "test2"}, header.Get("test"))
+	})
+}
+
+func TestSet(t *testing.T) {
+	assertion := assert.New(t)
+
+	t.Run("set single value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.Set("test", "test")
+		assertion.Equal([]string{"test"}, header.Get("test"))
+	})
+
+	t.Run("set multiple value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.Set("test", []string{"test1", "test2"})
+		assertion.Equal([]string{"test1", "test2"}, header.Get("test"))
+	})
+
+	t.Run("set integer value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		assertion.Panics(func() {
+			header.Set("test", 10)
+		})
+	})
+
+	t.Run("raw set", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.RawSet("test", "test1", "test2")
+		assertion.Equal([]string{"test1", "test2"}, header.Get("test"))
+	})
+}
+
+func TestRawGet(t *testing.T) {
+	assertion := assert.New(t)
+
+	t.Run("get multiple value", func(t *testing.T) {
+		header := NewHeader(metadata.New(nil))
+		header.Set("test", []string{"test1", "test2"})
+		assertion.Equal([]string{"test1", "test2"}, header.RawGet("test"))
+	})
+}
+
+func TestHeaderMerge(t *testing.T) {
+	assertion := assert.New(t)
+
+	t.Run("merge header", func(t *testing.T) {
+		header1 := NewHeader(metadata.New(nil))
+		header1.Set("test", []string{"test1", "test2"})
+		header1.Merge(nil)
+		assertion.Equal([]string{"test1", "test2"}, header1.Get("test"))
+
+		header2 := NewHeader(metadata.New(nil))
+		header2.Set("test", []string{"test3", "test4"})
+		header1.Merge(header2)
+		assertion.Equal([]string{"test1", "test2", "test3", "test4"}, header1.Get("test"))
+	})
+}
+
+func TestHeaderWalk(t *testing.T) {
+	assertion := assert.New(t)
+
+	h := NewHeader(metadata.New(nil))
+	h.Add("test1", "test1")
+	h.Add("test2", "test2")
+	h.Add("test3", "test3")
+
+	count := 0
+	h.Walk(func(key string, value interface{}) bool {
+		count++
+		return true
+	})
+	assertion.Equal(3, count)
+
+	count = 0
+	h.Walk(func(key string, value interface{}) bool {
+		count++
+		return false
+	})
+	assertion.Equal(1, count)
+}
+
+func TestHeaderClone(t *testing.T) {
+	assertion := assert.New(t)
+
+	t.Run("merge header", func(t *testing.T) {
+		header1 := NewHeader(metadata.New(nil))
+		header1.Set("test", []string{"test1", "test2"})
+		header2 := header1.Clone().(*Header)
+		assertion.Equal([]string{"test1", "test2"}, header2.Get("test"))
 	})
 }
