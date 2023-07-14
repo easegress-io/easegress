e8f9e8e36ddac95b4080cec71c96338b0767899e
sodaRyCN <757083350@qq.com>
2023-07-14 19:51:24 +0800

    increase cover rate

--- pkg/logger/lumberjack_test.go
+++ pkg/logger/lumberjack_test.go
@@ -864,12 +864,6 @@ func backupFileLocal(dir string) string {
 	return filepath.Join(dir, "foobar-"+fakeTime().Format(backupTimeFormat)+".log")
 }
 
-// logFileLocal returns the log file name in the given directory for the current
-// fake time using the local timezone.
-func logFileLocal(dir string) string {
-	return filepath.Join(dir, fakeTime().Format(backupTimeFormat))
-}
-
 // fileCount checks that the number of files in the directory is exp.
 func fileCount(dir string, exp int, t testing.TB) {
 	files, err := os.ReadDir(dir)
