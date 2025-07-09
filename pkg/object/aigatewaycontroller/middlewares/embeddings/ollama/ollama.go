1faadd84c92b3c7cb181d4e3099f647d6549cfef
chen <chen.su1994@outlook.com>
2025-07-09 16:32:22 +0800

    add ollama embedding

--- pkg/object/aigatewaycontroller/middlewares/embeddings/ollama/ollama.go
+++ pkg/object/aigatewaycontroller/middlewares/embeddings/ollama/ollama.go
@@ -17,25 +17,94 @@
 
 package ollama
 
-import "github.com/megaease/easegress/v2/pkg/object/aigatewaycontroller/middlewares/embeddings/embedtypes"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/megaease/easegress/v2/pkg/object/aigatewaycontroller/middlewares/embeddings/embedtypes"
+)
+
+const ollamaEmbedPath = "/api/embed"
 
 type (
 	ollamaEmbeddingHanlder struct {
 		spec *embedtypes.EmbeddingSpec
 	}
+
+	Duration struct {
+		time.Duration
+	}
+
+	// EmbedRequest is the reqeust of ollamd embedding api.
+	EmbedRequest struct {
+		Model string `json:"model"`
+
+		// Input is the input to embed, can be a string or an array of strings.
+		Input any `json:"input"`
+	}
+
+	// EmbedResponse is the response of ollamd embedding api.
+	EmbedResponse struct {
+		Model      string      `json:"model"`
+		Embeddings [][]float32 `json:"embeddings"`
+
+		TotalDuration   time.Duration `json:"total_duration,omitempty"`
+		LoadDuration    time.Duration `json:"load_duration,omitempty"`
+		PromptEvalCount int           `json:"prompt_eval_count,omitempty"`
+	}
 )
 
 func New(spec *embedtypes.EmbeddingSpec) embedtypes.EmbeddingHandler {
-	handler := &ollamaEmbeddingHanlder{}
+	handler := &ollamaEmbeddingHanlder{
+		spec: spec,
+	}
 	return handler
 }
 
 func (h *ollamaEmbeddingHanlder) EmbedDocuments(text string) ([]float32, error) {
-	// TODO
-	return nil, nil
+	embedReq := &EmbedRequest{
+		Model: h.spec.Model,
+		Input: text,
+	}
+	reqBody, err := json.Marshal(embedReq)
+	if err != nil {
+		return nil, err
+	}
+	u, err := url.JoinPath(h.spec.BaseURL, ollamaEmbedPath)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, u, bytes.NewReader(reqBody))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("ollama embedding request failed with status code %d, %s", resp.StatusCode, string(data))
+	}
+	var embedResp EmbedResponse
+	if err := json.Unmarshal(data, &embedResp); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	if len(embedResp.Embeddings) == 0 || len(embedResp.Embeddings[0]) == 0 {
+		return nil, fmt.Errorf("ollama embedding response is empty")
+	}
+	return embedResp.Embeddings[0], nil
 }
 
 func (h *ollamaEmbeddingHanlder) EmbedQuery(text string) ([]float32, error) {
-	// TODO
-	return nil, nil
+	return h.EmbedDocuments(text)
 }
