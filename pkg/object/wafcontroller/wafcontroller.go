984b77f76807b816c1c038e47df8ffa3606ba2ce
LokiWager <lokitingyi@gmail.com>
2025-08-04 19:28:14 -0400

    [enhancement] imp some methods

--- pkg/object/wafcontroller/wafcontroller.go
+++ pkg/object/wafcontroller/wafcontroller.go
@@ -18,7 +18,11 @@
 package wafcontroller
 
 import (
+	"fmt"
+	"github.com/megaease/easegress/v2/pkg/api"
 	"github.com/megaease/easegress/v2/pkg/context"
+	"github.com/megaease/easegress/v2/pkg/logger"
+	"strings"
 	"sync/atomic"
 
 	"github.com/megaease/easegress/v2/pkg/supervisor"
@@ -91,33 +95,91 @@ type (
 		ModSecurity string `json:"modSecurity,omitempty"` // ModSecurity rule string.
 		// Additional fields can be added here for custom rule configuration.
 	}
+
+	Status struct{}
 )
 
+func (spec *Spec) Validate() error {
+	names := make(map[string]struct{})
+	for _, group := range spec.RuleGroups {
+		if group.Name == "" {
+			return fmt.Errorf("RuleGroup name cannot be empty")
+		}
+		if _, exists := names[group.Name]; exists {
+			return fmt.Errorf("RuleGroup name must be unique: " + group.Name)
+		}
+		names[group.Name] = struct{}{}
+
+		for _, rule := range group.Rules {
+			if rule.SQLInjection.Enabled && len(rule.CustomRules) > 0 {
+				return fmt.Errorf("Cannot enable SQLInjection and CustomRules at the same time")
+			}
+			for _, customRule := range rule.CustomRules {
+				if customRule.Spec.ModSecurity == "" {
+					return fmt.Errorf("CustomRule ModSecurity cannot be empty")
+				}
+			}
+			// Additional validation for other rule types can be added here.
+		}
+	}
+
+	return nil
+}
+
 func (waf *WAFController) Category() supervisor.ObjectCategory {
-	//TODO implement me
-	panic("implement me")
+	return Category
 }
 
 func (waf *WAFController) Kind() string {
-	//TODO implement me
-	panic("implement me")
+	return Kind
 }
 
 func (waf *WAFController) DefaultSpec() interface{} {
-	//TODO implement me
-	panic("implement me")
+	return &Spec{
+		// Add default enabled rule groups or rules if needed.
+	}
 }
 
 func (waf *WAFController) Status() *supervisor.Status {
-	//TODO implement me
-	panic("implement me")
+	status := make(map[string]interface{})
+	// Here you can add status information about the WAFController.
+	return &supervisor.Status{
+		ObjectStatus: status,
+	}
 }
 
 func (waf *WAFController) Close() {
-	//TODO implement me
-	panic("implement me")
+	logger.Infof("Closing WAFController")
+	// Perform any necessary cleanup here.
 }
 
 func init() {
 	supervisor.Register(&WAFController{})
+	api.RegisterObject(&api.APIResource{
+		Category:    Category,
+		Kind:        Kind,
+		Name:        strings.ToLower(Kind),
+		Aliases:     aliases,
+		ValiateHook: validateHook,
+	})
+}
+
+func validateHook(operationType api.OperationType, spec *supervisor.Spec) error {
+	if operationType != api.OperationTypeCreate || spec.Kind() != Kind {
+		return nil
+	}
+
+	agcs := []string{}
+	supervisor.GetGlobalSuper().WalkControllers(func(controller *supervisor.ObjectEntity) bool {
+		if controller.Spec().Kind() == Kind {
+			agcs = append(agcs, controller.Spec().Name())
+		}
+		return true
+	})
+
+	if len(agcs) >= 1 {
+		return fmt.Errorf("only one WAFController is allowed, existed: %v", agcs)
+	}
+
+	return nil
 }
