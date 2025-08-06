26c23bc6d5d4a635992e6bb4b44a1a789cf1dc01
LokiWager <lokitingyi@gmail.com>
2025-08-06 11:45:52 -0400

    [enhancement] add api

---
+++ pkg/object/wafcontroller/api.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017, The Easegress Authors
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wafcontroller
+
+import (
+	"github.com/megaease/easegress/v2/pkg/api"
+	"net/http"
+)
+
+const (
+	APIGroupName = "waf"
+	APIPrefix    = "/waf"
+)
+
+func (waf *WAFController) registerAPIs() {
+	group := &api.Group{
+		Group: APIGroupName,
+		Entries: []*api.Entry{
+			{
+				Path:    APIPrefix + "/rule-groups/metrics",
+				Method:  "GET",
+				Handler: waf.getRuleGroupsMetrics,
+			},
+			{
+				Path:    APIPrefix + "/metrics",
+				Method:  "GET",
+				Handler: waf.metrics,
+			},
+		},
+	}
+
+	api.RegisterAPIs(group)
+}
+
+func (waf *WAFController) unregisterAPIs() {
+	api.UnregisterAPIs(APIGroupName)
+}
+
+func (waf *WAFController) getRuleGroupsMetrics(w http.ResponseWriter, r *http.Request) {
+	// This function should return the status of all rule groups.
+}
+
+func (waf *WAFController) metrics(w http.ResponseWriter, r *http.Request) {
+	// This function should return the statistics of the WAF controller.
+}
