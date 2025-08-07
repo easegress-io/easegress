165a3a532aaf7e5d9d2aceed5ca2e2e786c12ad0
chen <chen.su1994@outlook.com>
2025-08-07 16:00:38 +0800

    add waf process reqesut part

--- pkg/object/wafcontroller/rulegroup.go
+++ pkg/object/wafcontroller/rulegroup.go
@@ -18,6 +18,9 @@
 package wafcontroller
 
 import (
+	"fmt"
+	"net"
+
 	coreruleset "github.com/corazawaf/coraza-coreruleset/v4"
 	"github.com/corazawaf/coraza/v3"
 	"github.com/corazawaf/coraza/v3/types"
@@ -26,6 +29,7 @@ import (
 	"github.com/megaease/easegress/v2/pkg/context"
 	"github.com/megaease/easegress/v2/pkg/object/wafcontroller/protocol"
 	"github.com/megaease/easegress/v2/pkg/object/wafcontroller/rules"
+	"github.com/megaease/easegress/v2/pkg/protocols/httpprot"
 	"github.com/nacos-group/nacos-sdk-go/common/logger"
 )
 
@@ -117,8 +121,121 @@ func (rg *ruleGroup) Name() string {
 
 // Handle processes the request and returns a WAF response.
 func (rg *ruleGroup) Handle(ctx *context.Context) *protocol.WAFResult {
-	// TODO: Implement the logic to handle the request.
-	// First go through the preprocessors if any. For example, GEOIP lookups and put the result into the context.
-	// Then, prepare the request for WAF processing.
-	return nil
+	req := ctx.GetInputRequest().(*httpprot.Request)
+	tx := rg.waf.NewTransaction()
+	ctx.OnFinish(func() {
+		tx.ProcessLogging()
+		tx.Close()
+	})
+
+	if tx.IsRuleEngineOff() {
+		return &protocol.WAFResult{
+			Result: protocol.ResultOk,
+		}
+	}
+
+	// preprocessors
+	for _, preprocessor := range rg.preprocessors {
+		result := preprocessor(ctx, tx, req)
+		if result.Result != protocol.ResultOk {
+			return result
+		}
+	}
+
+	// process the request
+	result := rg.processRequest(ctx, tx, req)
+	if result.Result != protocol.ResultOk {
+		return result
+	}
+
+	// TODO: process the response
+
+	return &protocol.WAFResult{
+		Result: protocol.ResultOk,
+	}
+}
+
+func parseServerName(host string) string {
+	serverName, _, err := net.SplitHostPort(host)
+	if err != nil {
+		return host
+	}
+	return serverName
+}
+
+func formMessage(in *types.Interruption) string {
+	return fmt.Sprintf("WAF interruption: RuleID=%d, Action=%s, Status=%d, Data=%s",
+		in.RuleID, in.Action, in.Status, in.Data)
+}
+
+func (rg *ruleGroup) processRequest(_ *context.Context, tx types.Transaction, req *httpprot.Request) *protocol.WAFResult {
+	stdReq := req.Std()
+
+	var in *types.Interruption
+	tx.ProcessConnection(req.RealIP(), 0, "", 0)
+	tx.ProcessURI(stdReq.URL.String(), stdReq.Method, stdReq.Proto)
+
+	// process headers
+	for k, vs := range stdReq.Header {
+		for _, v := range vs {
+			tx.AddRequestHeader(k, v)
+		}
+	}
+	if stdReq.Host != "" {
+		tx.AddRequestHeader("Host", stdReq.Host)
+		tx.SetServerName(parseServerName(stdReq.Host))
+	}
+	if req.TransferEncoding != nil {
+		tx.AddRequestHeader("Transfer-Encoding", req.TransferEncoding[0])
+	}
+	in = tx.ProcessRequestHeaders()
+	if in != nil {
+		return &protocol.WAFResult{
+			Interruption: in,
+			Message:      formMessage(in),
+			Result:       protocol.ResultBlocked,
+		}
+	}
+
+	if tx.IsRequestBodyAccessible() {
+		if req.IsStream() {
+			// for streaming requests, we do not read the body or process it.
+			return &protocol.WAFResult{
+				Result: protocol.ResultOk,
+			}
+		}
+
+		it, _, err := tx.ReadRequestBodyFrom(req.GetPayload())
+		if err != nil {
+			return &protocol.WAFResult{
+				Message: fmt.Sprintf("failed to append request body: %s", err.Error()),
+				Result:  protocol.ResultError,
+			}
+		}
+		if it != nil {
+			return &protocol.WAFResult{
+				Interruption: it,
+				Message:      formMessage(it),
+				Result:       protocol.ResultBlocked,
+			}
+		}
+
+		it, err = tx.ProcessRequestBody()
+		if err != nil {
+			return &protocol.WAFResult{
+				Message: fmt.Sprintf("failed to process request body: %s", err.Error()),
+				Result:  protocol.ResultError,
+			}
+		}
+		if it != nil {
+			return &protocol.WAFResult{
+				Interruption: it,
+				Message:      formMessage(it),
+				Result:       protocol.ResultBlocked,
+			}
+		}
+	}
+	return &protocol.WAFResult{
+		Result: protocol.ResultOk,
+	}
 }
